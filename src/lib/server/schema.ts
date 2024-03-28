81cedbbcbeb5362c0ca60d1e8077af95cbeec44b
Stephen Buchanan <webtvmaster@gmail.com>
2024-03-28 14:03:15 +0100

    for some reason, one-to-many relation breaks type definitions

--- src/lib/server/schema.ts
+++ src/lib/server/schema.ts
@@ -9,14 +9,19 @@ import {
 } from "drizzle-orm/pg-core";
 // import { drizzle } from "drizzle-orm/node-postgres";
 
-export const plants = pgTable("plants", {
+export const plantsTable = pgTable("plants", {
   id: serial("id").primaryKey(),
   commonName: text("common_name"),
   description: text("description"),
   properties: json("properties"), // TODO: could be separate table, later
   imageUrl: text("image_url"),
+  parent1: integer("parent1_id"),
+  parent2: integer("parent2_id"),
 });
 
-export const plantsRelations = relations(plants, ({ many }) => ({
-  parents: many(plants),
+export const parentOne = relations(plantsTable, ({ many }) => ({
+  children: many(plantsTable, {
+    fields: [plantsTable.parent1],
+    references: [plantsTable.id],
+  }),
 }));
