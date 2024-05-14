fe9ce7bebf7af9f7d2a5e69d740e97a9d51e9506
hectoralbgs <132586368+hectoralbgs@users.noreply.github.com>
2024-05-14 12:44:53 +0200

    fixed backend query

--- src/routes/api/plants/[id]/+server.ts
+++ src/routes/api/plants/[id]/+server.ts
@@ -16,13 +16,8 @@ export const POST: RequestHandler = async ({ request }) => {
 export const PATCH: RequestHandler = async ({ request, params }) => {
   const plant = (await request.json()) as SelectPlant;
   console.log({ params, plant });
-  if (plant.imageUrl) {
-    await attachImageToPlant(plant.id, plant.imageUrl);
-    return json(plant, { status: 200 });
-  } else {
-    await updateWholePlant(plant.id, plant);
-    return json(plant, { status: 200 });
-  }
+  await updateWholePlant(plant.id, plant);
+  return json(plant, { status: 200 });
 };
 
 // export const GET: RequestHandler = async ({ request, params }) => {
