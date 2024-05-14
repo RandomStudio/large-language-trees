5ebaceabb5a3b8027431ca9aa9ffeaa290c202cb
hectoralbgs <132586368+hectoralbgs@users.noreply.github.com>
2024-05-14 11:03:02 +0200

    Changes popup display and corrects errors

--- src/routes/popupinfo.svelte
+++ src/routes/popupinfo.svelte
@@ -7,9 +7,18 @@
     <div class="overlay">
         <div class="popup">
             <button on:click={closePopup}>Fermer</button>
+            <br />
+            <img
+                src={plantDetails.imageUrl}
+                alt="Drawing of a {plantDetails.commonName}"
+            />
             <p>{plantDetails.commonName}</p>
             <p>{plantDetails.description}</p>
-            <p>{plantDetails.properties}</p>
+            <p>
+                The {plantDetails.commonName} has {plantDetails.properties
+                    .petalCountRange}
+                {plantDetails.properties.flowerColour} petals
+            </p>
         </div>
     </div>
 {/if}
