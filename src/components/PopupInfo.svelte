9e77feb70e02072ea7f293b3a8871e18e4008918
hectoralbgs <132586368+hectoralbgs@users.noreply.github.com>
2024-05-17 15:04:26 +0200

    If you click on a parent, you see its infos

--- src/components/PopupInfo.svelte
+++ src/components/PopupInfo.svelte
@@ -11,8 +11,14 @@
 
   export let closePopup: () => any;
 
-  const parent1 = data.seeds.find((plant) => plant.id === plantDetails.parent1);
-  const parent2 = data.seeds.find((plant) => plant.id === plantDetails.parent2);
+  let parent1 = data.seeds.find((plant) => plant.id === plantDetails.parent1);
+  let parent2 = data.seeds.find((plant) => plant.id === plantDetails.parent2);
+
+  function updatePlantDetails(plant: SelectPlant) {
+    plantDetails = plant;
+    parent1 = data.seeds.find((plant) => plant.id === plantDetails.parent1);
+    parent2 = data.seeds.find((plant) => plant.id === plantDetails.parent2);
+  }
 </script>
 
 <!-- svelte-ignore a11y-no-static-element-interactions -->
@@ -36,12 +42,14 @@
       <div class="parent1">
         <img src={parent1.imageUrl} alt="parent 1" />
         <p>{parent1.commonName}</p>
+        <button on:click={() => updatePlantDetails(parent1)}> See </button>
       </div>
     {/if}
     {#if parent2}
       <div class="parent2">
         <img src={parent2.imageUrl} alt="parent 2" />
         <p>{parent2.commonName}</p>
+        <button on:click={() => updatePlantDetails(parent2)}> See </button>
       </div>
     {/if}
     <p>{plantDetails.commonName}</p>
