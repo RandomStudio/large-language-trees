3aad201e0b76ed8091d1bc6ab163698246de9528
Stephen Buchanan <webtvmaster@gmail.com>
2024-05-24 16:39:44 +0200

    very rough grid

--- src/components/PlantCell.svelte
+++ src/components/PlantCell.svelte
@@ -10,65 +10,24 @@
 
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <div
-  class="cell"
   draggable={true}
   on:dragstart={(e) => {
     dispatchDragStart("dragStart", { e, gridIndex });
   }}
 >
-  <div class="draggable">
-    {#if data.imageUrl}
-      <img src={data.imageUrl} alt="the real plant" class="thumbnail" />
-    {:else}
-      <img src="/plants/placeholder.png" alt="placeholder" class="thumbnail" />
-    {/if}
-    <div class="plant-name">
-      {data.commonName}
+  <div class="relative">
+    <div class="absolute w-full">
+      <h1 class="text-center">
+        {data.commonName}
+      </h1>
     </div>
   </div>
+  {#if data.imageUrl}
+    <img src={data.imageUrl} alt="the real plant" />
+  {:else}
+    <img src="/plants/placeholder.png" alt="placeholder" />
+  {/if}
 </div>
 
 <style>
-  .cell {
-    width: 100%;
-    height: 100%;
-    cursor: pointer;
-    position: relative; /* Nécessaire pour le positionnement absolu des éléments à l'intérieur */
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .draggable {
-    width: 100%;
-    position: relative; /* Nécessaire pour positionner correctement l'image */
-    overflow: visible; /* Permet à l'image de dépasser */
-  }
-
-  .thumbnail {
-    position: absolute;
-    width: 120%; /* Agrandir l'image à 120% de sa cellule */
-    left: 50%; /* Centrer horizontalement */
-    transform: translate(
-      -50%,
-      -50%
-    ); /* Ajustement pour le centrage vertical et horizontal */
-    height: auto; /* Garder le ratio de l'image */
-    top: 50%; /* Centrer verticalement */
-  }
-
-  .plant-name {
-    display: none; /* Initially hidden */
-    position: absolute;
-    top: calc(100% + 13px);
-    left: 50%;
-    transform: translateX(-50%);
-    font-size: 0.6em;
-    white-space: nowrap;
-    overflow: visible;
-  }
-
-  .draggable:hover .plant-name {
-    display: block; /* Show on hover */
-  }
 </style>
