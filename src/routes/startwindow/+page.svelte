db8b92b0a2f6b0c5b55beb12115e57aa5c47ff11
hectoralbgs <132586368+hectoralbgs@users.noreply.github.com>
2024-05-31 12:13:37 +0200

    link the new function for displaying flowers

--- src/routes/startwindow/+page.svelte
+++ src/routes/startwindow/+page.svelte
@@ -2,22 +2,15 @@
     import { goto } from "$app/navigation";
     import {
         type SeedbankEntryWithPlant,
+        type GardenViewData,
         type MyGarden,
     } from "../../lib/types"; // Adjust the import paths as necessary
 
-    interface GardenViewData {
-        seeds: SeedbankEntryWithPlant[];
-        username: string;
-        garden: MyGarden;
-    }
-
     export let data: GardenViewData;
 
-    console.log(data.seeds);
+    import PlantDisplay from "../../components/PlantDisplay.svelte";
 
-    // Automatically select the first plant in the list to display
-    let selectedPlant = data.seeds.plantsInSeedbank[0].plant;
-    console.log(selectedPlant);
+    let selectedPlant = data.seedBank.plantsInSeedbank[0].plant;
 </script>
 
 <div class="min-h-screen bg-roel_green overflow-hidden">
@@ -31,19 +24,7 @@
         </p>
         <div class="text-left">
             {#if selectedPlant}
-                <div class="flex justify-center my-4">
-                    <img
-                        src={selectedPlant.imageUrl}
-                        alt={selectedPlant.commonName}
-                        style="width: 50%;"
-                    />
-                </div>
-                <p class="flex justify-center text-roel_blue">
-                    {selectedPlant.commonName}
-                </p>
-                <p class="text-roel_blue">
-                    {selectedPlant.description}
-                </p>
+                <PlantDisplay plant={selectedPlant} width="70%"></PlantDisplay>
             {:else}
                 <p class="text-roel_blue">No plants available</p>
             {/if}
