754963d9802c4f4d7b88a897a5bbfdc90e74e9e0
Stephen Buchanan <webtvmaster@gmail.com>
2024-06-04 11:45:07 +0200

    using q-floodfill

--- src/components/TransparencyMaker.svelte
+++ src/components/TransparencyMaker.svelte
@@ -8,8 +8,13 @@
   let canvasElement: HTMLCanvasElement;
 
   let finalImageData: Uint8ClampedArray | null = null;
+  let doUpload: boolean = false;
 
-  onMount(() => {
+  onMount(async () => {
+    const ff = await import("q-floodfill");
+    const FloodFill = ff.default;
+
+    console.log(window.innerWidth);
     const img = new Image();
     img.src = src;
     const ctx = canvasElement.getContext("2d");
@@ -22,28 +27,19 @@
           canvasElement.width,
           canvasElement.height
         );
-        const { data } = imageData;
         const topLeftColor = ctx.getImageData(0, 0, 1, 1).data;
         console.log({ topLeftColor });
-        let count = 0;
-        for (let i = 0; i < data.length; i += 4) {
-          if (
-            isWithinTolerance(
-              [data[i], data[i + 1], data[i + 2]],
-              [topLeftColor[0], topLeftColor[1], topLeftColor[2]],
-              tolerance
-            )
-          ) {
-            count++;
-            data[i + 3] = 0; // Set alpha to 0, making the pixel transparent.
-            // data[i] = 1;
-          }
-        }
-        console.log("altered", count, "pixels");
+
+        const floodFill = new FloodFill(imageData);
+        floodFill.fill("rgba(0,0,0,0)", 0, 0, tolerance);
+
+        const count = floodFill.modifiedPixelsCount;
+
+        console.log("modified", count, "pixels ok");
         ctx.putImageData(imageData, 0, 0);
 
         canvasElement.toBlob((blob) => {
-          if (blob) {
+          if (doUpload && blob) {
             console.log("look, a Blob:", blob);
             const formData = new FormData();
             formData.append("img", blob);
