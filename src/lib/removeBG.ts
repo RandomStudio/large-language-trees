cbce6132048da086b4cc9168917479f56a3a74af
JessicaKuerschner <68183487+JessicaKuerschner@users.noreply.github.com>
2024-06-04 12:31:46 +0200

    WIPDesignPollinatonPage

--- src/lib/removeBG.ts
+++ src/lib/removeBG.ts
@@ -1,5 +1,5 @@
-export function canvaWithoutBG(canvaId: string, imageId: string, imgSource: string) {
-    const canvas = document.getElementById(canvaId) as HTMLCanvasElement;
+export function canvaWithoutBG(canvaId, imgSource) {
+    const canvas = document.getElementById(canvaId);
     if (!canvas) return;
     const context = canvas.getContext("2d");
     if (!context) return;
@@ -8,23 +8,16 @@ export function canvaWithoutBG(canvaId: string, imageId: string, imgSource: stri
     img.onload = () => {
         canvas.width = 150;
         canvas.height = 150;
-        const scale = Math.min(
-            canvas.width / img.width,
-            canvas.height / img.height,
-        );
+        const scale = Math.min(canvas.width / img.width, canvas.height / img.height);
         const scaledWidth = img.width * scale;
         const scaledHeight = img.height * scale;
         const xOffset = (canvas.width - scaledWidth) / 2;
         const yOffset = (canvas.height - scaledHeight) / 2;
         context.drawImage(img, xOffset, yOffset, scaledWidth, scaledHeight);
+
         const topLeftColor = context.getImageData(0, 0, 1, 1).data;
         const tolerance = 10;
-        const imageData = context.getImageData(
-            0,
-            0,
-            canvas.width,
-            canvas.height,
-        );
+        const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
         const data = imageData.data;
 
         for (let i = 0; i < data.length; i += 4) {
@@ -32,21 +25,24 @@ export function canvaWithoutBG(canvaId: string, imageId: string, imgSource: stri
                 isWithinTolerance(
                     [data[i], data[i + 1], data[i + 2]],
                     [topLeftColor[0], topLeftColor[1], topLeftColor[2]],
-                    tolerance,
+                    tolerance
                 )
             ) {
                 data[i + 3] = 0; // Set alpha to 0, making the pixel transparent.
             }
+            if (
+                data[i] >= 250 && data[i + 1] >= 250 && data[i + 2] >= 250
+            ) {
+                data[i] = 0; data[i + 1] = 56; data[i + 2] = 255;
+            }
         }
         context.putImageData(imageData, 0, 0);
-        const pngUrl = canvas.toDataURL("image/png");
-        document.getElementById(imageId).src = pngUrl;
     };
 
-    img.src = imgSource; // Load the .png image
+    img.src = imgSource;
 }
 
-function isWithinTolerance(pixelColor: Array<number>, targetColor: Array<number>, tolerance: number) {
+function isWithinTolerance(pixelColor, targetColor, tolerance) {
     return (
         Math.abs(pixelColor[0] - targetColor[0]) <= tolerance &&
         Math.abs(pixelColor[1] - targetColor[1]) <= tolerance &&
