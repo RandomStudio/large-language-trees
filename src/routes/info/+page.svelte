79abd4744fae7706720c9b3caa5e8978bda96ff0
hectoralbgs <132586368+hectoralbgs@users.noreply.github.com>
2024-06-06 17:31:58 +0200

    Style info page

--- src/routes/info/+page.svelte
+++ src/routes/info/+page.svelte
@@ -5,13 +5,25 @@
 </script>
 
 <div
-  class="relative flex items-start justify-center min-h-screen bg-roel_green font-inter text-sm"
+  class="absolute flex items-start justify-center min-h-screen bg-roel_green font-inter text-sm"
 >
-  <button class="fixed top-5 right-10 text-roel_blue" on:click={goBack}>
-    ✕
+  <button
+    class="fixed top-8 right-7 text-roel_blue flex items-center justify-center w-10 h-10 border-2 border-roel_blue rounded-full"
+    on:click={goBack}
+  >
+    <svg
+      class="w-6 h-6 text-roel_blue"
+      fill="none"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path d="M15 19l-7-7 7-7" />
+    </svg>
   </button>
   <main class="mx-14 w-full max-w-4xl pt-16">
-    <!-- Padding top ajouté ici -->
     <div class="flex justify-center">
       <canvas id="canvas" style="display:none;"></canvas>
       <!-- svelte-ignore a11y-img-redundant-alt -->
