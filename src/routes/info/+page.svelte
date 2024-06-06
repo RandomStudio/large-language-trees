e2701f62f016252c7380e45609d96589ddf683db
hectoralbgs <132586368+hectoralbgs@users.noreply.github.com>
2024-06-06 17:25:41 +0200

    style of the info page

--- src/routes/info/+page.svelte
+++ src/routes/info/+page.svelte
@@ -1,42 +1,37 @@
 <script lang="ts">
-    function goBack() {
-        window.history.back();
-    }
+  function goBack() {
+    window.history.back();
+  }
 </script>
 
 <div
-    class="relative flex items-center justify-center min-h-screen bg-roel_green"
+  class="relative flex items-start justify-center min-h-screen bg-roel_green font-inter text-sm"
 >
-    <button
-        class="fixed top-10 right-10 text-roel_blue text-2xl"
-        on:click={goBack}
-    >
-        ✕
-    </button>
-    <main class="mx-14 w-full max-w-4xl">
-        <div class="flex justify-center">
-            <canvas id="canvas" style="display:none;"></canvas>
-            <!-- svelte-ignore a11y-img-redundant-alt -->
-            <img src="/titleimg.png" alt="logo" style="display:block;" />
-        </div>
+  <button class="fixed top-5 right-10 text-roel_blue" on:click={goBack}>
+    ✕
+  </button>
+  <main class="mx-14 w-full max-w-4xl pt-16">
+    <!-- Padding top ajouté ici -->
+    <div class="flex justify-center">
+      <canvas id="canvas" style="display:none;"></canvas>
+      <!-- svelte-ignore a11y-img-redundant-alt -->
+      <img src="/titleimg.png" alt="logo" style="display:block;" />
+    </div>
 
-        <div class="text-left">
-            <p class="text-roel_blue">
-                Welcome to the common garden of Lucullus. This Digital Common
-                Garden is about connection and cross-pollination.<br /><br />
-                Find other Gardeners to start cross-breeding and witness the offspring
-                flourish in the common garden of Lucullus.
-            </p>
-            <br />
-            <div class="text-left">
-                <span class="text-roel_blue font-semibold">Production:</span><br
-                />
-                <span class="text-roel_blue">Studio Random</span><br />
-                <span class="text-roel_blue font-semibold">Image Data:</span><br
-                />
-                <span class="text-roel_blue">Chat GPT/ Open AI</span><br /><br
-                />
-            </div>
-        </div>
-    </main>
+    <div class="text-left">
+      <p class="text-roel_blue">
+        Welcome to Randoms Lucullan Gardenia, the exclusive greenhouse,
+        sprouting digital abundance. <br /><br />
+        Find other Gardeners in the studio to start cross-pollinating and witness
+        your sprouts flourish in our common garden.
+      </p>
+      <br />
+      <div class="text-left">
+        <span class="text-roel_blue font-semibold">Production:</span><br />
+        <span class="text-roel_blue">Studio Random</span><br />
+        <span class="text-roel_blue font-semibold">Image Data:</span><br />
+        <span class="text-roel_blue">Chat GPT/ Open AI</span><br /><br />
+      </div>
+    </div>
+  </main>
 </div>
