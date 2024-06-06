d593b16ca7d8fec0f1f8a48a53723403bdffc3f5
jonatanmodin <124877254+jonatanmodin@users.noreply.github.com>
2024-06-06 14:35:34 +0200

    Placement of new "parent"-plants as new user join, according to existing logic

--- src/routes/garden/+page.svelte
+++ src/routes/garden/+page.svelte
@@ -3,12 +3,14 @@
     GardenPlantEntryWithPlant,
     GardenViewData,
     PlantProperties,
+    SelectPlant,
   } from "$lib/types";
   export let data: GardenViewData;
 
   let alt = "alt placeholder";
 
   //distribution on screen and size of plants in database
+  const numberOfInitialPlants = 35;
   const rootScale = 2; //maths root not plant roots lol
   const minPlantHeight = Math.pow(0.1, 1 / rootScale);
   const maxPlantHeight = Math.pow(30, 1 / rootScale);
@@ -37,7 +39,7 @@
   const plantPositionsX: number[] = [];
   const plantPositionsY: number[] = [];
   let crowdednessTolerance = 0; //dont change this use initial instead
-  const initialCrowdednessTolerance = 150;
+  const initialCrowdednessTolerance = 250;
 
   //constants relating to adding new plants
   const newPlantIDs: string[] = [];
@@ -45,6 +47,7 @@
 
   import { onMount } from "svelte";
   onMount(() => {
+    //pushInitialPlants();
     importNewPlants();
   });
 
@@ -52,14 +55,14 @@
     return low2 + ((high2 - low2) * (value - low1)) / (high1 - low1);
   }
 
-  function plantHeight(plant: GardenPlantEntryWithPlant): number {
-    return (plant.plant.properties as PlantProperties)["high(m)"] as number;
+  function plantHeight(plant: SelectPlant): number {
+    return (plant.properties as PlantProperties)["high(m)"] as number;
   }
 
-  function rootScaleFromPlantHeight(plant: GardenPlantEntryWithPlant): number {
+  function rootScaleFromPlantHeight(plant: SelectPlant): number {
     return Math.min(
       Math.pow(
-        (plant.plant.properties as PlantProperties)["high(m)"] as number,
+        (plant.properties as PlantProperties)["high(m)"] as number,
         1 / rootScale,
       ),
       30,
@@ -70,7 +73,7 @@
     return mapRange(value, lowIn, highIn, low2, high2);
   }
 
-  function placePlantOnYAxis(plant: GardenPlantEntryWithPlant) {
+  function placePlantOnYAxis(plant: SelectPlant) {
     return (
       frameSize +
       topBorder +
@@ -84,7 +87,7 @@
     );
   }
 
-  function placePlantOnXAxis(plant: GardenPlantEntryWithPlant) {
+  function placePlantOnXAxis(plant: SelectPlant) {
     let proposedPlantPositionX = 0;
     let isSpaceOk = false;
 
@@ -97,7 +100,7 @@
     }
 
     // console.log(crowdednessTolerance); // use to check what crowdedness level each plant landed on
-    //plantIDs.push(plant.plant.id); // make comment to check if importNewPlants() works
+    plantIDs.push(plant.id); // make comment to check if importNewPlants() works
     plantPositionsX.push(proposedPlantPositionX);
     plantPositionsY.push(placePlantOnYAxis(plant));
     return proposedPlantPositionX;
@@ -111,7 +114,7 @@
   }
 
   function checkIfSpaceIsOk(
-    plant: GardenPlantEntryWithPlant,
+    plant: SelectPlant,
     proposedPlantPositionX: number,
   ) {
     let distanceList = [];
@@ -128,7 +131,7 @@
   }
 
   function calculateDistance(
-    plant: GardenPlantEntryWithPlant,
+    plant: SelectPlant,
     proposedPlantPositionX: number,
     x: number,
     y: number,
@@ -148,7 +151,7 @@
 
   async function importNewPlants() {
     const response = await fetch("http://localhost:5173/api/plants");
-    const newPlants = await response.json(); // get all plants info
+    const newPlants = (await response.json()) as SelectPlant[]; // get all plants info
     for (let i = 0; i < newPlants.length; i++) {
       newPlantIDs.push(newPlants[i].id); // push all plant IDs into list
       newPlantParent1.push(newPlants[i].id); // push all plant parent1 into list
@@ -167,29 +170,49 @@
       } else {
         children.push(confirmedNewPlantIDs[i]); // if parent of new plant is anything else, it is the result of a cross-breeding
       }
-      for (let i = 0; i < parents.length; i++) {
-        addParents(parents[i]);
-        plantIDs.push(parents[i]);
-      }
-      for (let i = 0; i < children.length; i++) {
-        addChildren(children[i]);
-        plantIDs.push(children[i]);
-      }
     }
-    console.log(parents);
-    console.log(children);
+    for (let i = 0; i < parents.length; i++) {
+      addParents(parents[i], newPlants);
+      //plantIDs.push(parents[i]);
+    }
+    for (let i = 0; i < children.length; i++) {
+      addChildren(children[i]);
+      //plantIDs.push(children[i]);
+    }
   }
-  function addParents(parent: string) {
-    // add parents to DOM
-    const para = document.createElement("p");
-    const node = document.createTextNode(parent);
-    para.appendChild(node);
 
-    const element = document.getElementById("container");
-    if (element) {
-      element.appendChild(para);
+  function addParents(plantToAddId: string, newPlants: SelectPlant[]) {
+    const plant = newPlants.find((item: any) => item.id === plantToAddId); //find the plant by ID
+
+    if (plant) {
+      const img = document.createElement("img");
+      img.src = plant.imageUrl ?? "";
+
+      // set the style and position
+      img.className = "fixed skew-animated";
+      img.style.marginTop = `${placePlantOnYAxis(plant) + (Math.random() - 0.5) * randomnessY}px`;
+      img.style.marginLeft = `${placePlantOnXAxis(plant)}px`;
+      img.style.width = `${
+        minPlantHeightOutput +
+        remapPlantHeight(
+          rootScaleFromPlantHeight(plant),
+          minPlantHeight,
+          maxPlantHeight,
+        ) *
+          (maxPlantHeightOutput - minPlantHeightOutput)
+      }px`;
+      img.style.zIndex = `${3000 - plantHeight(plant) * 100}`;
+      img.style.height = "auto";
+      img.style.animationDuration = "8s";
+
+      const element = document.getElementById("container");
+      if (element) {
+        element.appendChild(img);
+      } else {
+        console.error("Element with ID 'container' not found.");
+      }
     } else {
-      console.error("Element with ID 'container' not found.");
+      console.error(`Plant with ID '${plantToAddId}' not found.`);
     }
   }
   function addChildren(children: string) {
@@ -199,7 +222,7 @@
 
 <body>
   <div id="container" class="fixed top-0 left-0 w-screen h-screen">
-    {#each data.garden.plantsInGarden as plant}
+    {#each data.garden.plantsInGarden.slice(0, numberOfInitialPlants) as plant}
       <!-- svelte-ignore a11y-click-events-have-key-events -->
       <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
       <img
@@ -207,23 +230,23 @@
         {alt}
         class="fixed skew-animated"
         on:load={() => {
-          console.log();
+          console.log("initial plant");
         }}
         style:width={minPlantHeightOutput +
           remapPlantHeight(
-            rootScaleFromPlantHeight(plant),
+            rootScaleFromPlantHeight(plant.plant),
             minPlantHeight,
             maxPlantHeight,
           ) *
             (maxPlantHeightOutput - minPlantHeightOutput) +
           "px"}
-        style:margin-top={placePlantOnYAxis(plant) +
+        style:margin-top={placePlantOnYAxis(plant.plant) +
           (Math.random() - 0.5) * randomnessY +
           "px"}
-        style:margin-left={placePlantOnXAxis(plant) + "px"}
+        style:margin-left={placePlantOnXAxis(plant.plant) + "px"}
         style:animation-duration={animationLength + "s"}
         style:animation-delay={randomAnimationDelay() + "s"}
-        style:z-index={3000 - plantHeight(plant) * 100}
+        style:z-index={3000 - plantHeight(plant.plant) * 100}
       />
     {/each}
   </div>
@@ -246,6 +269,6 @@
   }
 
   .skew-animated {
-    animation: skew-animation infinite;
+    animation: skew-animation 8s infinite;
   }
 </style>
