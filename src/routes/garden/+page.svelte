8867621ccb32805ecb62af82afe2e445b0571fc1
jonatanmodin <124877254+jonatanmodin@users.noreply.github.com>
2024-06-10 09:29:02 +0200

    Import children and place in line going upwards

--- src/routes/garden/+page.svelte
+++ src/routes/garden/+page.svelte
@@ -3,9 +3,11 @@
     GardenPlantEntryWithPlant,
     GardenViewData,
     PlantProperties,
-    SelectPlant
+    SelectPlant,
   } from "$lib/types";
   export let data: GardenViewData;
+
+  import { onMount } from "svelte";
 
   //constants relating to making plants not overlap
   const initialCrowdednessTolerance = 250;
@@ -27,7 +29,11 @@
   const smallestPlant = 0.1;
   const minPlantHeight = Math.pow(smallestPlant, 1 / rootScale);
   const maxPlantHeight = Math.pow(tallestPlant, 1 / rootScale);
+  const defaultValue = 500; // default value in case child cant find parents
   const randomnessY = 20; // random displacement in y direction
+
+  // relating to double placement of children
+  const parentList = ["parent1", "parent2"];
 
   //constants relating to remap function
   const low2 = 0;
@@ -37,31 +43,44 @@
     plant: SelectPlant;
     x: number;
     y: number;
+    numberOfChildren: number;
   }
 
   let displayPlants: PositionedPlant[] = [];
 
-  if (data && data.garden && data.garden.plantsInGarden) {
-    data.garden.plantsInGarden.forEach((entry) => {
-      if (entry && entry.plant) {
-        if (entry.plant.parent1 === null) {
-          const newPositionedPlant: PositionedPlant = {
-            plant: entry.plant,
-            x: parentX(entry.plant),
-            y: parentY(entry.plant)
-          };
-          displayPlants.push(newPositionedPlant); // adds plant to list if its a parent
-        } else {
-          const newPositionedPlant: PositionedPlant = {
-            plant: entry.plant,
-            x: childX(entry.plant),
-            y: childY(entry.plant)
-          };
-          displayPlants.push(newPositionedPlant); // adds plant to list if its a child
-        }
+  function addPlants(plant: SelectPlant) {
+    if (plant.parent1 === null) {
+      const newPositionedPlant: PositionedPlant = {
+        plant: plant,
+        x: parentX(plant),
+        y: parentY(plant),
+        numberOfChildren: 0,
+      };
+      displayPlants.push(newPositionedPlant); // adds plant to list if it's a parent
+    } else {
+      for (let i = 0; i < parentList.length; i++) {
+        increaseNumberOfChildren(plant, parentList[i]);
+        const newPositionedPlant: PositionedPlant = {
+          plant: plant,
+          x: childX(plant, parentList[i]),
+          y: childY(plant, parentList[i]),
+          numberOfChildren: 0,
+        };
+        displayPlants.push(newPositionedPlant); // adds plant to list if it's a child
       }
-    });
+    }
   }
+
+  // reversed list because test child came first in list and couldnt find its parents because they werent added yet
+  if (data && data.garden && data.garden.plantsInGarden) {
+    data.garden.plantsInGarden
+      .slice()
+      .reverse()
+      .forEach((entry) => {
+        addPlants(entry.plant);
+      });
+  }
+
   function parentX(plant: SelectPlant) {
     let proposedX = 0;
     let spaceOk = false;
@@ -71,7 +90,6 @@
       proposedX = frameSize + Math.random() * (monitorWidth - frameSize * 2);
       spaceOk = checkIfSpaceIsOk(plant, proposedX, tolerance);
       tolerance -= 1;
-      console.log(tolerance);
     }
     return proposedX;
   }
@@ -86,22 +104,42 @@
           remapPlantHeight(
             Math.log(rootScaleFromPlantHeight(plant)) + 1,
             Math.log(minPlantHeight) + 1,
-            Math.log(maxPlantHeight) + 1
+            Math.log(maxPlantHeight) + 1,
           ))
     );
   }
 
-  function childX(plant: SelectPlant) {
-    return 100;
+  function childX(plant: SelectPlant, parentName: string) {
+    return findParent(plant, parentName)?.x ?? defaultValue;
   }
-  function childY(plant: SelectPlant) {
-    return 100;
+  function childY(plant: SelectPlant, parentName: string) {
+    return (
+      (findParent(plant, parentName)?.y ?? defaultValue) -
+      (findParent(plant, parentName)?.numberOfChildren ?? defaultValue) * 30
+    );
+  }
+
+  function findParent(plant: SelectPlant, parentName: string) {
+    return displayPlants.find(
+      (p) => p.plant.id === plant[parentName as keyof SelectPlant],
+    );
+  }
+
+  function increaseNumberOfChildren(plant: SelectPlant, parentName: string) {
+    const parent = displayPlants.find(
+      (p) => p.plant.id === plant[parentName as keyof SelectPlant],
+    );
+    if (parent) {
+      parent.numberOfChildren++;
+    } else {
+      console.log(`Plant not found`);
+    }
   }
 
   function checkIfSpaceIsOk(
     plant: SelectPlant,
     proposedX: number,
-    tolerance: number
+    tolerance: number,
   ) {
     let distanceList = [];
     for (let i = 0; i < displayPlants.length; i++) {
@@ -114,7 +152,7 @@
   function calculateDistance(
     plant: SelectPlant,
     proposedX: number,
-    displayPlants: PositionedPlant
+    displayPlants: PositionedPlant,
   ): number {
     let plantX = proposedX;
     let plantY = parentY(plant);
@@ -132,7 +170,7 @@
       remapPlantHeight(
         rootScaleFromPlantHeight(plant),
         minPlantHeight,
-        maxPlantHeight
+        maxPlantHeight,
       ) *
         (maxPlantHeightOutput - minPlantHeightOutput)
     );
@@ -143,11 +181,6 @@
     return (plant.properties as PlantProperties)["high(m)"] as number;
   }
 
-  // function to give give plant z-index
-  function zIndexFunction(plant: SelectPlant) {
-    return 3000 - plantHeight(plant) * 100;
-  }
-
   // remap plant height to 0 -> 1
   function remapPlantHeight(value: any, lowIn: any, highIn: any) {
     return low2 + ((high2 - low2) * (value - lowIn)) / (highIn - lowIn);
@@ -156,7 +189,7 @@
   function rootScaleFromPlantHeight(plant: SelectPlant): number {
     return Math.max(
       smallestPlant,
-      Math.min(Math.pow(plantHeight(plant), 1 / rootScale), tallestPlant)
+      Math.min(Math.pow(plantHeight(plant), 1 / rootScale), tallestPlant),
     );
   }
 
@@ -164,6 +197,28 @@
     return (Math.random() * -animationLength) / 3;
   }
   let alt = "alt placeholder";
+
+  //continously add new plants
+  async function importNewPlants() {
+    console.log("importing");
+    const response = await fetch("http://localhost:5173/api/plants");
+    const newPlants = (await response.json()) as SelectPlant[]; // get all plants info
+    let confirmedNewPlants = displayPlants.filter(
+      (item) => !displayPlants.includes(item), // isolate plants that are new
+    );
+    confirmedNewPlants.forEach((entry) => {
+      console.log("New plant found!");
+      addPlants(entry.plant);
+    });
+  }
+
+  onMount(() => {
+    const intervalId = setInterval(importNewPlants, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  });
 </script>
 
 <body>
@@ -172,15 +227,19 @@
       <!-- svelte-ignore a11y-click-events-have-key-events -->
       <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
       <img
+        on:click={() => {
+          importNewPlants();
+        }}
         src={plant.plant.imageUrl}
         {alt}
         class="fixed skew-animated"
         style:margin-left={plant.x - scaleFunction(plant.plant) / 2 + "px"}
         style:margin-top={plant.y - scaleFunction(plant.plant) + "px"}
         style:width={scaleFunction(plant.plant) + "px"}
-        style:z-index={zIndexFunction(plant.plant)}
-        style:animation-duration={animationLength + "s"}
-        style:animation-delay={randomAnimationDelay() + "s"}
+        style:z-index={Math.round(plant.y)}
+        style:--skew-animation-delay={(Math.random() * -animationLength) / 3 +
+          "s"}
+        style:--skew-animation-length={animationLength + "s"}
       />
     {/each}
   </div>
@@ -202,7 +261,24 @@
     }
   }
 
+  @keyframes birth-animation {
+    0% {
+      scale: 0;
+      filter: blur(9000);
+      filter: contrast(5);
+    }
+
+    100% {
+      scale: 1;
+      filter: blur(0);
+      filter: contrast(1);
+    }
+  }
+
   .skew-animated {
-    animation: skew-animation infinite;
+    animation:
+      skew-animation var(--skew-animation-length) infinite
+        var(--skew-animation-delay),
+      birth-animation 2s ease-out;
   }
 </style>
