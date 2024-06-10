c89b935b53ef19f008ad2df46da1c6ca687c0958
hectoralbgs <132586368+hectoralbgs@users.noreply.github.com>
2024-06-10 16:18:04 +0200

    updating the info page

--- src/routes/info/+page.svelte
+++ src/routes/info/+page.svelte
@@ -4,46 +4,36 @@
   }
 </script>
 
-<div
-  class="absolute flex items-start justify-center min-h-screen bg-roel_green font-inter text-sm"
+<button
+  class="fixed top-8 right-7 z-50 text-roel_blue flex items-center justify-center w-10 h-10 border-2 border-roel_blue rounded-full"
+  on:click={goBack}
 >
-  <button
-    class="fixed top-8 right-7 text-roel_blue flex items-center justify-center w-10 h-10 border-2 border-roel_blue rounded-full"
-    on:click={goBack}
+  <svg
+    class="w-6 h-6 text-roel_blue"
+    fill="none"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
   >
-    <svg
-      class="w-6 h-6 text-roel_blue"
-      fill="none"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      stroke-width="2"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-    >
-      <path d="M15 19l-7-7 7-7" />
-    </svg>
-  </button>
-  <main class="mx-14 w-full max-w-4xl pt-16">
-    <div class="flex justify-center">
-      <canvas id="canvas" style="display:none;"></canvas>
-      <!-- svelte-ignore a11y-img-redundant-alt -->
-      <img src="/titleimg.png" alt="logo" style="display:block;" />
-    </div>
+    <path d="M15 19l-7-7 7-7" />
+  </svg>
+</button>
 
-    <div class="text-left">
-      <p class="text-roel_blue">
-        Welcome to Randoms Lucullan Gardenia, the exclusive greenhouse,
-        sprouting digital abundance. <br /><br />
-        Find other Gardeners in the studio to start cross-pollinating and witness
-        your sprouts flourish in our common garden.
-      </p>
-      <br />
-      <div class="text-left">
-        <span class="text-roel_blue font-semibold">Production:</span><br />
-        <span class="text-roel_blue">Studio Random</span><br />
-        <span class="text-roel_blue font-semibold">Image Data:</span><br />
-        <span class="text-roel_blue">Chat GPT/ Open AI</span><br /><br />
-      </div>
-    </div>
-  </main>
+<div class="mx-12 pt-32 font-inter text-left text-roel_blue text-sm">
+  <img src="/titleimg.png" alt="logo" />
+  <p class="mt-4">
+    Welcome to Randoms Lucullan Gardenia, the exclusive greenhouse, sprouting
+    digital abundance. <br /><br />
+    Find other Gardeners in the studio to start cross-pollinating and witness your
+    sprouts flourish in our common garden.
+  </p>
+  <br />
+  <div>
+    <span class="font-semibold">Production:</span><br />
+    <span>Studio Random</span><br />
+    <span class="font-semibold">Image Data:</span><br />
+    <span>Chat GPT/ Open AI</span>
+  </div>
 </div>
