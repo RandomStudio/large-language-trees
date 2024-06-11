fcea3d4979b854e7849844cdc21aabb0c8a10c83
hectoralbgs <132586368+hectoralbgs@users.noreply.github.com>
2024-06-11 14:21:41 +0200

    deleting the .play for the video

--- src/routes/pollination/+page.svelte
+++ src/routes/pollination/+page.svelte
@@ -49,7 +49,6 @@
 
     videoElement.srcObject = stream;
     videoElement.setAttribute("playsinline", "true"); // Required to tell iOS safari we don't want fullscreen
-    videoElement.play();
 
     codeReader.decodeFromStream(stream, videoElement, (result, err) => {
       if (result && !busy) {
