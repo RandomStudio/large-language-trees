ebc32249898906d413a2cb7b7d4a49cc11c6fe69
hectoralbgs <132586368+hectoralbgs@users.noreply.github.com>
2024-06-13 12:31:29 +0200

    Adjusting the waiting spinner

--- src/components/WaitingSpinner.svelte
+++ src/components/WaitingSpinner.svelte
@@ -6,6 +6,15 @@
 />
 
 <style>
+  @keyframes spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+
   .slow-reverse-spin {
     animation: spin 4s linear infinite reverse; /* 4 seconds for slower spin and reverse direction */
   }
