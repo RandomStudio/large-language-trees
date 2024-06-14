c16fb6c52c42a609031e7c7b9c7047dcf7588ed0
Stephen Buchanan <webtvmaster@gmail.com>
2024-06-14 15:35:02 +0200

    users api should not return user data

--- src/routes/api/users/+server.ts
+++ src/routes/api/users/+server.ts
@@ -3,5 +3,5 @@ import { json, type RequestHandler } from "@sveltejs/kit";
 
 export const GET: RequestHandler = async ({}) => {
   const users = await db.query.users.findMany();
-  return json(users, { status: 200 });
+  return json({ count: users.length }, { status: 200 });
 };
