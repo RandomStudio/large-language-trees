3c2c01b551de4d603004358986f7b4d522bcd82c
jonatanmodin <124877254+jonatanmodin@users.noreply.github.com>
2024-06-17 15:39:10 +0200

    Padding fixes

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -49,7 +49,7 @@
       </div>
       <div class="mt-4 text-center">
         <button
-          class="bg-roel_green text-roel_blue font-inter text-xl px-4 py-2 border-2 w-11/12 max-w-xs border-roel_blue rounded-full active:bg-roel_blue active:text-roel_green"
+          class="bg-roel_green text-roel_blue font-inter text-xl px-4 py-2 mb-5 border-2 w-11/12 max-w-xs border-roel_blue rounded-full active:bg-roel_blue active:text-roel_green"
           on:click={() => handleClick(index)}
         >
           Start Pollinating
