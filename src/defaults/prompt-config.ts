970dc10c6b549ed8e5d38171dc6c87b3e30d84b5
Hector <hector.albiges@random.studio>
2024-07-15 17:11:54 +0200

    previous prompt

--- src/defaults/prompt-config.ts
+++ src/defaults/prompt-config.ts
@@ -23,13 +23,13 @@ const DefaultPrompt: PromptConfig = {
       label: "Preamble",
       description:
         "Give the AI a scenario, or a general idea of how they should approach your instructions, or what persona they should take on.",
-      text: "You are a knowledgeable botanist"
+      text: "Imagine you are a knowledgeable botanist with a flair for descriptive writing, and an interest in science fiction and celebrities."
     },
     explanation: {
       label: "Explanation",
       description:
         "Lay out exactly what you are going to provide (the two parent plants), the data format that will be used (JSON) and what the AI needs to do with it (combine properties, etc.). The actual properties of each parent plant will follow directly after this.",
-      text: 'You managed to cross-pollinate two plants. Don\'t worry about whether this is physically possible in real life, but feel free to speculate on the likely outcome. I will describe each parent plant in JSON form, giving its common name and by a brief description, followed by a set of key-value pairs for "properties". With the information about the two parent plants, try to come up with a plausible new plant that would result from the cross-pollination.'
+       text: 'I manage to cross-pollinate two plants. Don\'t worry about whether this is physically possible in real life, but feel free to speculate on the likely outcome. I will describe each parent plant in JSON form, giving its common name and by a brief description, followed by a set of key-value pairs for "properties". With the information about the two parent plants, try to come up with a plausible new plant that would result from the cross-pollination.'
     },
     instructions: {
       label: "Final Instructions",
@@ -41,7 +41,7 @@ const DefaultPrompt: PromptConfig = {
   image: {
     model: "dall-e-3",
     instructions:
-      "Create a precise pixel art 8-bit image based on the following description. The background is pure white, with no text, no stars and the plant is standing alone in the middle. Here is the description:"
+    "You are a botanist with a love for pixel art. Create an 8 bit pixel art image for a botanical game on a solely white background in the art style of Stardew Valley, based on the description that follows. Make sure there is no texts the image. Just the plant in the middle of the image. The plant is a result of a crossbreeding between two plants. Make sure the names of the parent plants are reflected in the created image. Don't forget the background color of the image should be #FFFFFF pure white. Here is the description:"
   }
 };  
 
