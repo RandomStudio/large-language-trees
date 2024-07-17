07c9f41bb45dd01a4dc1ebcb4974c63bdc2868c3
Stephen Buchanan <webtvmaster@gmail.com>
2024-07-17 14:27:33 +0200

    much shorter default image prompt

--- src/defaults/prompt-config.ts
+++ src/defaults/prompt-config.ts
@@ -25,7 +25,7 @@ const DefaultPrompt: PromptConfig = {
   image: {
     model: "dall-e-3",
     instructions:
-      "You are a botanist with a love for pixel art. Create an 8 bit pixel art image for a botanical game on a solely white background in the art style of Stardew Valley, based on the description that follows. Make sure there is no text in the image - only the plant in the middle of the image. The plant is a result of a crossbreeding between two plants. Make sure the names of the parent plants are reflected in the created image. Don't forget the background color of the image should be #FFFFFF (pure white). Here is the description:"
+      "Create an 8 bit pixel art image for a botanical game on a solid white background. Here is the description:"
   }
 };
 
