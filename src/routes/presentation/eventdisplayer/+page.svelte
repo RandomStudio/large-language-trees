fd577381a98f63f81b16198e285db355b7592a00
Hector <hector.albiges@random.studio>
2024-07-24 10:54:36 +0200

    Name of pollinationevent page

--- src/routes/presentation/eventdisplayer/+page.svelte
+++ src/routes/presentation/eventdisplayer/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   //import BreakingNews from "./BreakingNews.svelte";
   import PollinationResult from "./PollinationResult.svelte";
-  //import PollinationEvent from "./pollinationevent.svelte";
+  //import PollinationEvent from "./PollinationEvent.svelte";
 </script>
 
 <main>
