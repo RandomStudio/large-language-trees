9c3370fca20ae1b7be492e59b1b1d62791568fad
Hector <hector.albiges@random.studio>
2024-07-24 11:20:47 +0200

    Small adjustments

--- src/routes/presentation/eventdisplayer/BreakingNews.svelte
+++ src/routes/presentation/eventdisplayer/BreakingNews.svelte
@@ -1,42 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-  <body class="bg-roel_rose">
-    <div
-      class="w-full h-[350px] flex text-center items-center justify-center text-roel_rose text-6xl font-jeanb bg-roel_purple"
+<body class="bg-roel_rose">
+  <div
+    class="w-full h-[350px] flex text-center items-center justify-center text-roel_rose text-6xl font-jeanb bg-roel_purple"
+  >
+    ROELSCOOTER'S <br /> LAVENDER
+  </div>
+
+  <div class="fill-current absolute top-1/2">
+    <svg
+      width="540"
+      height="320"
+      xmlns="http://www.w3.org/2000/svg"
+      class="text-roel_purple"
     >
-      ROELSCOOTER'S
-      <br /> LAVENDER
-    </div>
+      <path d="M10 10 H580 V200 A290 145 0 0 1 10 200 Z" />
+    </svg>
+  </div>
 
-    <div class="fill-current absolute top-1/2">
-      <svg
-        width="540"
-        height="320"
-        xmlns="http://www.w3.org/2000/svg"
-        class="text-roel_purple"
-      >
-        <path d="M10 10 H580 V200 A290 145 0 0 1 10 200 Z" />
-      </svg>
-    </div>
+  <div class="absolute">
+    <!-- svelte-ignore a11y-img-redundant-alt -->
+    <img
+      src="/58.png"
+      alt="Lavender Image"
+      class="place-content-center h-auto"
+    />
+  </div>
 
-    <div class="absolute">
-      <!-- svelte-ignore a11y-img-redundant-alt -->
-      <img
-        src="/58.png"
-        alt="Lavender Image"
-        class="place-content-center h-auto"
-      />
-    </div>
-
-    <div
-      class="w-full h-[350px] flex text-center items-center justify-center absolute bottom-0 text-roel_purple text-6xl font-jeanb bg-roel_rose"
-    >
-      JUST SPROUTED
-      <br />IN THE GARDEN!
-    </div>
-
-    <script lang="ts">
-      // Your TypeScript code here
-    </script>
-  </body>
-</html>
+  <div
+    class="w-full h-[350px] flex text-center items-center justify-center absolute bottom-0 text-roel_purple text-6xl font-jeanb bg-roel_rose"
+  >
+    JUST SPROUTED <br /> IN THE GARDEN!
+  </div>
+</body>
--- src/routes/presentation/eventdisplayer/PollinationResult.svelte
+++ src/routes/presentation/eventdisplayer/PollinationResult.svelte
@@ -1,42 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-  <body class="bg-roel_green">
-    <div
-      class="w-full h-[350px] flex text-center items-center justify-center text-roel_green text-6xl font-jeanb bg-roel_purple"
+<body class="bg-roel_green">
+  <div
+    class="w-full h-[350px] flex text-center items-center justify-center text-roel_green text-6xl font-jeanb bg-roel_purple"
+  >
+    THE
+    <br /> LAVENDOSE
+  </div>
+
+  <div class="absolute top-1/2">
+    <svg
+      width="540"
+      height="320"
+      fill="None"
+      viewBox="0 0 540 300"
+      xmlns="http://www.w3.org/2000/svg"
     >
-      THE
-      <br /> LAVENDOSE
-    </div>
-
-    <div class="fill-current absolute top-1/2">
-      <svg
-        width="540"
-        height="320"
-        xmlns="http://www.w3.org/2000/svg"
-        class="text-roel_purple"
-      >
-        <path d="M10 10 H580 V200 A290 145 0 0 1 10 200 Z" />
-      </svg>
-    </div>
-
-    <div class="absolute">
-      <!-- svelte-ignore a11y-img-redundant-alt -->
-      <img
-        src="/59.png"
-        alt="Lavender Image"
-        class="place-content-center h-auto"
+      <path
+        d="M0 0H1000V301C1000 577.142 776.142 801 500 801C223.858 801 0 577.142 0 301V0Z"
+        fill="#4B0082"
       />
-    </div>
+    </svg>
+  </div>
 
-    <div
-      class="w-full h-[350px] flex text-center items-center justify-center absolute bottom-0 text-roel_green text-6xl font-jeanb bg-roel_purple"
-    >
-      RESULTING
-      <br />IN...
-    </div>
+  <div class="absolute">
+    <!-- svelte-ignore a11y-img-redundant-alt -->
+    <img
+      src="/59.png"
+      alt="Lavender Image"
+      class="place-content-center h-auto"
+    />
+  </div>
 
-    <script lang="ts">
-      // Your TypeScript code here
-    </script>
-  </body>
-</html>
+  <div
+    class="w-full h-[350px] flex text-center items-center justify-center absolute bottom-0 text-roel_green text-6xl font-jeanb bg-roel_purple"
+  >
+    RESULTING
+    <br />IN...
+  </div>
+</body>
