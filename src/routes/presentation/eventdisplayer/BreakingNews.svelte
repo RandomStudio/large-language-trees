b3c94dbf1609625c00cf9f01259a0dee8065afbf
Hector <hector.albiges@random.studio>
2024-07-24 11:58:14 +0200

    Adjust the breakingnews event

--- src/routes/presentation/eventdisplayer/BreakingNews.svelte
+++ src/routes/presentation/eventdisplayer/BreakingNews.svelte
@@ -1,31 +1,20 @@
 <body class="bg-roel_rose">
   <div
-    class="w-[540px] h-[350px] flex text-center items-center justify-center text-roel_rose text-6xl font-jeanb bg-roel_purple"
+    class="w-full h-[350px] flex text-center items-center justify-center text-roel_rose text-6xl font-jeanb bg-roel_purple"
   >
     ROELSCOOTER'S <br /> LAVENDER
   </div>
 
-  <div class="fill-roel_purple absolute top-1/2 w-[540px] h-auto">
-    <svg
-      viewBox="0 0 1000 801"
-      width="540"
-      height="432"
-      xmlns="http://www.w3.org/2000/svg"
-      class="text-roel_purple"
-    >
+  <div class="fill-roel_purple absolute top-1/2 w-full h-auto">
+    <svg viewBox="0 0 1000 801" xmlns="http://www.w3.org/2000/svg">
       <path
         d="M0 0H1000V301C1000 577.142 776.142 801 500 801C223.858 801 0 577.142 0 301V0Z"
       />
     </svg>
   </div>
 
-  <div class="absolute">
-    <!-- svelte-ignore a11y-img-redundant-alt -->
-    <img
-      src="/58.png"
-      alt="Lavender Image"
-      class="place-content-center h-auto"
-    />
+  <div class="fixed inset-0 flex items-center justify-center">
+    <img src="/58.png" alt="Lavender" />
   </div>
 
   <div
