60fb8a54e85a52913efe3143af81081322dd4434
JessicaKuerschner <68183487+JessicaKuerschner@users.noreply.github.com>
2024-07-24 16:38:57 +0200

    StatusFeed, BRollDetail, BRollPan, BRollZoom, Statistics, Statistics2

---
+++ src/routes/presentation/eventdisplayer/BRollZoomOut.svelte
@@ -0,0 +1,12 @@
+<body class="bg-roel_purple">
+  <div
+    class="w-full h-[350px] flex text-center items-center justify-center text-roel_purple text-7xl font-jeanb bg-roel_rose"
+  >
+    ROELSCOOTER'S <br /> GARDEN
+  </div>
+  <div
+    class="absolute bottom-5 w-full flex justify-center text-roel_rose text-4xl font-primer"
+  >
+    Join the Garden!
+  </div>
+</body>
---
+++ src/routes/presentation/eventdisplayer/TopList.svelte
@@ -0,0 +1,40 @@
+<body class="bg-roel_purple h-screen flex flex-col justify-end">
+  <div class="w-full">
+    <div
+      class="w-full h-[230px] flex items-center text-left pl-5 text-roel_purple text-7xl font-jeanb bg-roel_rose"
+    >
+      MOST ACTIVE
+      <br />POLLINATORS:
+    </div>
+    <div
+      class="w-full h-[80px] flex items-center justify-between px-5 bg-roel_purple"
+    >
+      <div class="text-roel_rose text-4xl font-primer">RoelScooters</div>
+      <div class="text-roel_rose text-4xl font-primer">x 90</div>
+    </div>
+    <div
+      class="w-full h-[80px] flex items-center justify-between px-5 bg-roel_rose"
+    >
+      <div class="text-roel_purple text-4xl font-primer">SteveB</div>
+      <div class="text-roel_purple text-4xl font-primer">x 63</div>
+    </div>
+    <div
+      class="w-full h-[80px] flex items-center justify-between px-5 bg-roel_purple"
+    >
+      <div class="text-roel_rose text-4xl font-primer">OuiOuiBaguette</div>
+      <div class="text-roel_rose text-4xl font-primer">x 42</div>
+    </div>
+    <div
+      class="w-full h-[80px] flex items-center justify-between px-5 bg-roel_rose"
+    >
+      <div class="text-roel_purple text-4xl font-primer">Jompy5</div>
+      <div class="text-roel_purple text-4xl font-primer">x 12</div>
+    </div>
+    <div
+      class="w-full h-[80px] flex items-center justify-between px-5 bg-roel_purple"
+    >
+      <div class="text-roel_rose text-4xl font-primer">JessicaK</div>
+      <div class="text-roel_rose text-4xl font-primer">x 3</div>
+    </div>
+  </div>
+</body>
