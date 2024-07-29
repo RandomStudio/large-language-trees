68f998b08a46424eeda8dc0ac2ecef45c996ac3c
Stephen Buchanan <webtvmaster@gmail.com>
2024-07-29 16:03:53 +0200

    undoing client-side force

--- src/routes/presentation/display/[id]/+page.ts
+++ src/routes/presentation/display/[id]/+page.ts
@@ -1,15 +1,11 @@
-import type { DisplayEventContents } from "$lib/events.types";
 import type { PageLoad } from "./$types";
 import type { PresentationDisplayFrontendState } from "./types";
 
 export const load: PageLoad = ({
-  params,
-  url
+  params
 }): PresentationDisplayFrontendState => {
-  const forceMode = url.searchParams.get("mode");
   return {
     id: params.id,
-    contents: { name: "idle", contents: null },
-    forceMode
+    contents: { name: "idle", contents: null }
   };
 };
--- src/routes/presentation/display/[id]/types.ts
+++ src/routes/presentation/display/[id]/types.ts
@@ -2,5 +2,4 @@ import type { DisplayEventContents } from "$lib/events.types";
 export interface PresentationDisplayFrontendState {
   id: string;
   contents: DisplayEventContents;
-  forceMode?: string;
 }
