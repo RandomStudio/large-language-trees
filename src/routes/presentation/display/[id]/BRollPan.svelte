2cd0b1a2c1ba04d0d39cf51967a87a4d34693c7e
Hector <hector.albiges@random.studio>
2024-07-29 16:18:05 +0200

    Display Garden component and BrollPan window

--- src/routes/presentation/display/[id]/BRollPan.svelte
+++ src/routes/presentation/display/[id]/BRollPan.svelte
@@ -1,13 +1,26 @@
-<body class="bg-roel_purple">
-  <div class="absolute bottom-5 left-40 text-roel_green text-4xl font-primer">
+<script>
+  import DisplayGarden from "./DisplayGarden.svelte";
+  import { garden } from "./gardenExample.js";
+</script>
+
+<body class="relative bg-roel_purple min-h-screen">
+  <DisplayGarden xGarden={0} yGarden={0} height={300} width={230} {garden}
+  ></DisplayGarden>
+  <DisplayGarden xGarden={280} yGarden={320} height={300} width={200} {garden}
+  ></DisplayGarden>
+  <DisplayGarden xGarden={0} yGarden={640} height={300} width={200} {garden}
+  ></DisplayGarden>
+  <DisplayGarden xGarden={280} yGarden={960} height={300} width={200} {garden}
+  ></DisplayGarden>
+
+  <div class="fixed bottom-5 left-40 text-roel_green text-4xl font-primer">
     Join the Garden!
   </div>
-  <div class="absolute left-5 bottom-5 w-28 h-auto">
-    <!-- svelte-ignore a11y-img-redundant-alt -->
+  <div class="fixed left-5 bottom-5 w-28 h-auto">
     <img
       src="/livinggarden_QR_purple_green.png"
       alt="Barcode"
-      class="place-content-center h-auto"
+      class="w-full h-auto"
     />
   </div>
 </body>
