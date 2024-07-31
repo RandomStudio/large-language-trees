ec20819e3fbe6116c9ff9e82212cbdead0a3e015
JessicaKuerschner <68183487+JessicaKuerschner@users.noreply.github.com>
2024-07-31 15:17:56 +0200

    WIP_rework

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -11,10 +11,14 @@
   }
 </script>
 
-<img src="FlowerDuo.png" class="absolute -z-10 opacity-100" alt="FlowerDuo" />
+<img
+  src="FlowerDuo.png"
+  class="absolute -z-10 opacity-50 h-3/5 left-8"
+  alt="FlowerDuo"
+/>
 <div class="mx-10 pt-[15vh] font-primer text-6xl text-roel_blue">
   <p class="font-jeanb text-center">LET'S<br />POLLINATE</p>
-  <div class="text-center">
+  <div class="text-center top-3">
     <form method="post" use:enhance class="mt-4 text-center max-w-md">
       <input
         class="bg-roel_green/75 border-[3px] border-roel_blue rounded-full text-roel_blue font-primer text-3xl px-4 py-[0.5rem] w-full max-w-xs placeholder-roel_blue placeholder:font-primer"
@@ -44,18 +48,17 @@
   </div>
 
   {#if form?.message}
-    <div class="text-roel_blue text-left">
+    <div class="text-roel_blue text-left text-base ml-4">
       Error: {form.message}
     </div>
   {/if}
 </div>
 <div
-  class="font-primer text-left mt-32 text-base mx-0 text-roel_green bg-roel_blue"
+  class="font-primer text-left mt-5 text-base mx-0 text-roel_green bg-roel_blue"
 >
   <div class="p-10">
     <p>
-      Welcome to Random's synergistic Lusthof, where digital abundance is
-      sprouting.
+      Welcome to the Garden of Random, where digital abundance is sprouting.
       <br />
       <br /> Find other Gardeners in the studio to start cross-pollinating and witness
       your sprouts flourish in our communal garden.
--- src/routes/app/info/+page.svelte
+++ src/routes/app/info/+page.svelte
@@ -8,35 +8,25 @@
 
 <ReturnButton functionReturn={goBack}></ReturnButton>
 
-<div class="mx-10 pt-20 font-primer text-left text-roel_blue text-base mb-20">
-  <img src="/titleimg_new.png" alt="logo" />
-  <p class="mt-12">
+<div class="mx-10 pt-20 font-primer text-left text-roel_blue mb-20">
+  <p class="font-jeanb text-center text-6xl">LET'S<br />POLLINATE</p>
+  <p class="mt-12 text-base">
     What is this project about?
     <br />
-    <br /> The "Let's Pollinate" app seeks to invert the typical narrative of
+    <br /> The Let's Pollinate app seeks to invert the typical narrative of
     digital technology as a force of isolation and detachment. Instead of
-    pulling us deeper into the virtual world, Lusthof leverages technology to
-    foster real-world connections and appreciation for the natural world. This
-    approach envisions technology not as an isolating force, but as a catalyst
-    for tangible, meaningful interactions.
-    <br /><br />In The Garden, each user begins with a single seed, representing
-    the potential for growth and connection. To enrich the communal garden and
-    diversify their seed collection, users must engage with other gardeners. The
-    combination of the Large Language Model (LLM) Chat-GPT and the text-to-image
-    model DALL-E synthesises the characteristics of two plants to create a new,
-    unique seedling.
-    <br /><br />
-    Initially, the app reflects classical garden aesthetics, where individual spaces
-    are distinctly separated. These early stages of the garden echo historical views
-    of gardens as paradises—exclusive, controlled environments that showcase human
-    mastery over nature. However, as more plants are added, the garden evolves into
-    a more chaotic, uncontrolled space. This transformation prompts critical reflection
-    on the extent of control we exert through technology and within the garden itself.
-    It challenges us to reconsider whether such control is desirable or necessary.
-    <br /><br />Blending Digital and Natural Worlds <br /><br />The evolution
-    from order to chaos mirrors broader questions about technology's role. By
-    blending digital innovation with real-world interactions, Lusthof invites
-    users to consider the balance between technological control and the vitality
-    of the natural world.
+    pulling us deeper into the virtual world, Let's Pollinate leverages
+    technology to foster real-world connections and appreciation for the natural
+    world. This approach envisions technology not as an isolating force, but as
+    a catalyst for tangible, meaningful interactions.
+    <br /><br />Each user begins with a single seed, representing the potential
+    for growth and connection. To enrich the communal garden and diversify their
+    seed collection, users must engage with other gardeners. The combination of
+    the Large Language Model (LLM) Chat-GPT and the text-to-image model DALL-E
+    synthesises the characteristics of two plants to create a new, unique
+    seedling.
+    <br /><br /> Through the act of creating something unique together and playful
+    graphics, each connection becomes something special and is being celebrated in
+    the community.
   </p>
 </div>
