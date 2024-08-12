ae62cf99016c5b1afe96e786d986f03af4fd4b4f
jonatanmodin <124877254+jonatanmodin@users.noreply.github.com>
2024-08-12 15:43:21 +0200

    Animated Plant

--- src/components/PlantDisplay.svelte
+++ src/components/PlantDisplay.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import type { SelectPlant } from "$lib/types";
   import { page } from "$app/stores";
+  import AnimatedPlant from "../components/AnimatedPlant.svelte";
 
   export let plant: SelectPlant;
   export let applyFilters: boolean = false;
@@ -19,6 +20,7 @@
     class:contrast-200={applyFilters}
     class:mix-blend-difference={applyFilters}
   />
+  <AnimatedPlant imageURL={plant.imageUrl || ""} />
   {#if $page.url.pathname !== "/app/startwindow"}
     <p class="text-roel_blue font-primer text-3xl">
       {plant.commonName}
