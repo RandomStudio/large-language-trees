4e5c5a44affd7d2c388e196b6cb0462ad494a88b
jonatanmodin <124877254+jonatanmodin@users.noreply.github.com>
2024-08-21 14:29:42 +0200

    Commented out the "Date of discovery" in order to get PopupInfo working again

--- src/components/PopupInfo.svelte
+++ src/components/PopupInfo.svelte
@@ -22,12 +22,12 @@
 <div class="fixed top-0 left-0 right-0 bottom-0 bg-roel_green overflow-auto">
   <div class="mx-10 font-primer text-roel_blue text-left mt-20 mb-20">
     <PlantDisplay plant={plantDetails} applyFilters={false} />
-    {#if !isOriginalPlant}
+    <!--{#if !isOriginalPlant}
       <p class="text-base mt-4 text-center">
         Date of discovery : {plantDetails.created.getDate()}/{plantDetails.created.getUTCMonth() +
           1}/{plantDetails.created.getFullYear()} at {plantDetails.created.getHours()}:{plantDetails.created.getMinutes()}
       </p>
-    {/if}
+    {/if}-->
     <p class="text-base mt-4">
       {plantDetails.description}
     </p>
