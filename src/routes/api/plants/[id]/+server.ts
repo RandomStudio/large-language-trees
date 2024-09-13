45308d67c7c943f589f0b4dfc4804362aae225a0
Stephen Buchanan <webtvmaster@gmail.com>
2024-09-13 11:55:57 +0200

    oops, shouldn't have removed this

---
+++ src/routes/api/plants/[id]/+server.ts
@@ -0,0 +1,27 @@
+import { db } from "$lib/server/db";
+import { plants } from "$lib/server/schema";
+import { updateWholePlant } from "$lib/server";
+import type { SelectPlant } from "$lib/types";
+import { json, type RequestHandler } from "@sveltejs/kit";
+import { eq } from "drizzle-orm";
+
+export const PATCH: RequestHandler = async ({ request, params }) => {
+  const plant = (await request.json()) as SelectPlant;
+  console.log({ params, plant });
+  await updateWholePlant(plant.id, plant);
+  return json(plant, { status: 200 });
+};
+
+export const GET: RequestHandler = async ({ params }) => {
+  const id = params["id"];
+  if (id) {
+    const plant = await db.query.plants.findFirst({ where: eq(plants.id, id) });
+    if (plant) {
+      return json(plant, { status: 200 });
+    } else {
+      throw Error("plant not found");
+    }
+  } else {
+    throw Error("Param missing");
+  }
+};
