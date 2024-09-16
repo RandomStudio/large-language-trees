e839f382ae9984dfdfe3802c382456009e574e17
Stephen Buchanan <webtvmaster@gmail.com>
2024-09-16 16:50:21 +0200

    vite config for mkcert

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
-import mkcert from 'vite-plugin-mkcert'
+import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
   plugins: [sveltekit(), mkcert()],
@@ -11,6 +11,7 @@ export default defineConfig({
     }
   },
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
+    proxy: {}
   }
 });
