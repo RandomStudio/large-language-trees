af6b57030892dc64920ad05f7c1a5438a4915afb
Stephen Buchanan <webtvmaster@gmail.com>
2024-09-18 17:07:52 +0200

    new layout for PollinationResult

--- src/routes/presentation/dynamic/[id]/PollinationResult.svelte
+++ src/routes/presentation/dynamic/[id]/PollinationResult.svelte
@@ -1,22 +1,11 @@
 <script lang="ts">
   import { onMount } from "svelte";
-  import { getColors } from "./findColors";
   import type { InsertPlant, PublicUserInfo, SelectPlant } from "$lib/types";
-  import { POLLINATION_START_TIMEOUT } from "$lib/constants";
-  import { fade } from "svelte/transition";
-  import { FADE_DURATION } from "$lib/constants";
   import PlantDisplay from "$lib/shared-components/PlantDisplay.svelte";
 
   export let authorTop: PublicUserInfo;
   export let authorBottom: PublicUserInfo;
-  export let newPlant: InsertPlant;
-
-  let brightColorNew = "rgb(255, 185, 198)";
-  let darkColorNew = "rgb(117, 0, 147)";
-  let brightColorTop = "rgb(255, 185, 198)";
-  let darkColorTop = "rgb(117, 0, 147)";
-  let brightColorBottom = "rgb(255, 185, 198)";
-  let darkColorBottom = "rgb(117, 0, 147)";
+  export let newPlant: SelectPlant;
 
   const soundFiles = [
     "/Sound 1 - Schuup.mp3",
@@ -34,17 +23,29 @@
 
   onMount(() => {
     const audio = new Audio(getRandomSoundFile());
-    audio.play();
+    // audio.play();
   });
 </script>
 
-<div class="fixed inset-0 flex items-center justify-center">
-  <div class="absolute w-screen h-screen flex items-center justify-center">
-    {authorTop}<br />
+<div class="w-screen h-screen items-center justify-center standard-gradient">
+  <div
+    class="w-screen text-center text-roel_yellow text-4xl font-gyst absolute top-32 z-10 uppercase"
+  >
+    {authorTop.username}<br />
     &
-    {authorBottom}
+    {authorBottom.username}
+  </div>
+
+  <div
+    class="grid grid-cols-1 gap-1 place-content-center place-items-center h-screen w-screen"
+  >
     <PlantDisplay imageUrl={newPlant.imageUrl || ""} applyFilters={false} />
-    gave life to:<br />
+  </div>
+
+  <div
+    class="w-screen text-center text-new_purple text-4xl font-gyst absolute bottom-32 z-10 uppercase"
+  >
+    Gave life to<br />
     {newPlant.commonName}
   </div>
 </div>
