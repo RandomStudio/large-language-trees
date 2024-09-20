b581b494761cc9e64d3c6365fcc6e35f5d229898
Stephen Buchanan <webtvmaster@gmail.com>
2024-09-20 14:16:45 +0200

    can fix statusfeed ok

--- src/routes/presentation/+layout.svelte
+++ src/routes/presentation/+layout.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
   import "../../index.css";
+
+  import { page } from "$app/stores";
+
+  const isFixed = $page.url.searchParams.has("fixed");
 </script>
 
-<div class="fixed top-0 left-0">
+<div class:fixed-size={isFixed}>
   <slot />
 </div>
+
+<style>
+  .fixed-size {
+    width: 540px;
+    height: 1620px;
+  }
+</style>
--- src/routes/presentation/fixed/leaderboard/+page.svelte
+++ src/routes/presentation/fixed/leaderboard/+page.svelte
@@ -50,9 +50,9 @@
 {#if data.gardensWithPlantCounts.length === 0 || data.topGardenWithPlants.plants.length === 0}
   <Idle />
 {:else}
-  <div class="w-screen h-screen items-center justify-center standard-gradient">
+  <div class="w-100 h-100 items-center justify-center standard-gradient">
     <div
-      class="w-screen text-center text-roel_yellow text-4xl font-gyst absolute top-32 z-10 uppercase"
+      class="w-100 text-center text-roel_yellow text-4xl font-gyst absolute top-32 z-10 uppercase"
     >
       Most Active<br />
       Pollinators
@@ -65,7 +65,7 @@
     />
 
     <div
-      class="w-screen text-center text-new_purple text-4xl font-gyst absolute bottom-32 z-10 uppercase"
+      class="w-100 text-center text-new_purple text-4xl font-gyst absolute bottom-32 z-10 uppercase"
     >
       {#each data.gardensWithPlantCounts as { user, count }}
         <div>
