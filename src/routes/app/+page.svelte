9e8761a365e0b827ac7d7b1c933cbf778880be59
Andrew Hill <contact@andrewthomashill.co.uk>
2024-09-23 11:14:37 +0200

    Do not allow users to select buttons

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -79,7 +79,7 @@
               data-umami-event="Start button"
               data-test="start-button"
               class="text-roel_green bg-roel_blue font-primer text-3xl px-4 py-[0.5rem] mt-4 w-full max-w-xs border-[3px] border-roel_blue rounded-full active:bg-roel_blue active:text-roel_green {isSubmitting &&
-                'opacity-50'}"
+                'opacity-50'} select-none"
               type="submit"
               disabled={isSubmitting}
             >
