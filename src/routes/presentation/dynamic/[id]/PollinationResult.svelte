eb00e29c4beea749101b1e5c210328f9ff406e38
Stephen Buchanan <webtvmaster@gmail.com>
2024-09-25 15:37:50 +0200

    PollinationResult layout fixed

--- src/routes/presentation/dynamic/[id]/PollinationResult.svelte
+++ src/routes/presentation/dynamic/[id]/PollinationResult.svelte
@@ -29,21 +29,25 @@
 
 <div class="w-full h-full items-center justify-center standard-gradient">
   <div
-    class="w-full text-center text-roel_yellow text-4xl font-gyst absolute top-32 z-10 uppercase"
+    class="w-full text-center text-roel_yellow text-7xl font-gyst absolute top-32 z-10 uppercase"
   >
-    {authorTop.username}<br />
-    &
-    {authorBottom.username}
+    <div>
+      {authorTop.username}
+    </div>
+    <div>&</div>
+    <div>
+      {authorBottom.username}
+    </div>
   </div>
 
   <div
-    class="grid grid-cols-1 gap-1 place-content-center place-items-center h-full w-full"
+    class="grid grid-cols-1 gap-1 place-content-center place-items-center h-screen w-full"
   >
     <PlantDisplay imageUrl={newPlant.imageUrl || ""} applyFilters={false} />
   </div>
 
   <div
-    class="w-full text-center text-new_purple text-4xl font-gyst absolute bottom-32 z-10 uppercase"
+    class="w-full text-center text-new_purple text-7xl font-gyst absolute bottom-32 z-10 uppercase"
   >
     Gave life to<br />
     {newPlant.commonName}
