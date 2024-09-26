16ee45c1b9300989e81afd34cfe39115b40028d2
Stephen Buchanan <webtvmaster@gmail.com>
2024-09-26 16:51:22 +0200

    back button better aligned vertically

--- src/lib/shared-components/TopRightButton.svelte
+++ src/lib/shared-components/TopRightButton.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <button
-  class="fixed top-3 right-6 z-30 text-roel_green font-primer flex bg-new_purple items-center justify-center w-10 h-10 border-[2px] border-roel_green rounded-full active:bg-roel_blue active:text-roel_green"
+  class="fixed top-7 right-4 z-30 text-roel_green font-primer flex bg-new_purple items-center justify-center w-10 h-10 border-[2px] border-roel_green rounded-full active:bg-roel_blue active:text-roel_green"
   on:click={onClick}
 >
   <slot />
